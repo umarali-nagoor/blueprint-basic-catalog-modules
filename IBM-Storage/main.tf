9a82deb39a41d8ac311c597117f2124499d69d1a
Stephen Strutt <steve_strutt@uk.ibm.com>
2022-07-02 14:46:23 +0100

    Update main.tf

--- IBM-Storage/main.tf
+++ IBM-Storage/main.tf
@@ -17,14 +17,21 @@ variable "cos_storage_class" {
   default     = "smart"
 }
 
-variable "cos_single_site_loc" {
+variable "cos_storage_plan" {
   type        = string
-  default     = "sjc04"
+  description = "COS plan"
+  default     = "standard"
+}
+
+
+variable "cos_single_site_loc" {
+  type    = string
+  default = "sjc04"
 }
 
 variable "cos_bucket_name" {
-  type        = string
-  default     = "demo-cos-buck"
+  type    = string
+  default = "demo-cos-buck"
 }
 
 resource "random_id" "name" {
@@ -46,18 +53,18 @@ output "cos_crn" {
 resource "ibm_resource_instance" "cos_instance" {
   name              = var.cos_instance_name
   service           = "cloud-object-storage"
-  plan              = "standard"
+  plan              = var.cos_storage_plan
   location          = "global"
   resource_group_id = var.resource_group_id
-  tags = ["t1", "t2"]
+  tags              = ["t1", "t2"]
 
 }
 
 
 resource "ibm_cos_bucket" "standard-ams03" {
-  bucket_name           = "${var.cos_bucket_name}-${random_id.name.hex}"
-  resource_instance_id  = ibm_resource_instance.cos_instance.id
-  single_site_location  = var.cos_single_site_loc
-  storage_class         = var.cos_storage_class  
+  bucket_name          = "${var.cos_bucket_name}-${random_id.name.hex}"
+  resource_instance_id = ibm_resource_instance.cos_instance.id
+  single_site_location = var.cos_single_site_loc
+  storage_class        = var.cos_storage_class
 }
 
